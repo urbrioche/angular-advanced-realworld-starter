11c03d003f86ae57fc39e79c285ef4ee27562861
jerry <aafulin@gmail.com>
2022-07-31 09:55:11 +0800

    任務1：LayoutModule/ LayoutComponent - 修正上一版

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {LayoutComponent} from "./layout/layout.component";
 
 const routes: Routes = [
-  {path: '', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule)}
+  {path: '', component: LayoutComponent},
 ];
 
 @NgModule({
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
   declarations: [
     LayoutComponent
   ],
+  exports: [
+    LayoutComponent
+  ],
   imports: [
     CommonModule,
-    LayoutRoutingModule
+    RouterModule
   ]
 })
 export class LayoutModule { }
