9ac5d7c17ded993cfcd6f8e8d25d0c34ef54f237
jerry <aafulin@gmail.com>
2022-07-31 15:51:24 +0800

    任務 07: Reactive Forms 驗證表單資料

--- src/app/posts/create/create.component.ts
+++ src/app/posts/create/create.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   templateUrl: './create.component.html',
@@ -7,9 +7,13 @@ import {FormBuilder} from '@angular/forms';
 })
 export class CreateComponent implements OnInit {
   form = this.fb.group({
-    title: this.fb.control(''),
+    title: this.fb.control('', {
+      validators: [Validators.required]
+    }),
     description: this.fb.control(''),
-    body: this.fb.control(''),
+    body: this.fb.control('', {
+      validators: [Validators.required, Validators.minLength(10)]
+    }),
     tags: this.fb.array([
       this.fb.control('Angular'),
       this.fb.control('HTML'),
