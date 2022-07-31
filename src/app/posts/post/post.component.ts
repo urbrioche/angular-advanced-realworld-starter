8676f23e9aeaeff0b79df3fc172fe8c2b34c5da7
jerry <aafulin@gmail.com>
2022-07-31 13:09:27 +0800

    任務 03: 單篇文章連結至/post頁面

--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostService} from "../../post.service";
+import {ActivatedRoute} from "@angular/router";
+import {Article} from "../../interfaces/article";
 
 @Component({
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+  article?: Article;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService) {
+  }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(param => {
+        const id = param.get('id') || '';
+        this.postService.getArticle(id).subscribe(result => {
+          this.article = result.article;
+        });
+      }
+    )
   }
 
 }
